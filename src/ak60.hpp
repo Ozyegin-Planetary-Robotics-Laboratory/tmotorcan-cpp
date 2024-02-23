00f202262eb8d8e9234ff02d2287449416bd79bc
Toprak Efe Akkılıç <efe.akkilic@ozu.edu.tr>
2024-02-23 15:36:21 +0300

    Added read routine to get real-time data from motors.

--- src/ak60.hpp
+++ src/ak60.hpp
@@ -1,12 +1,12 @@
 /**
  * @file ak60.hpp
  * @author Toprak Efe Akkılıç (efe.akkilic@ozu.edu.tr)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2024-02-22
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 
 #ifndef H_AK60_HPP
@@ -27,7 +27,11 @@
 
 namespace TMotor
 {
-    
+
+const uint8_t enterMotorControlMsg[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFC};
+const uint8_t exitMotorControlMsg[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFD};
+const uint8_t zeroMotorControlMSG[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFE};
+
 enum MotorMode {
   DUTY = 0x00000000,
   CURRENTLOOP = 0x00000100,
@@ -37,6 +41,16 @@ enum MotorMode {
   SETORIGIN = 0x00000500,
   POSITIONVELOCITY = 0x00000600
 };
+
+enum MotorFault {
+  NONE = 0x00000000,
+  OVERTEMPERATURE,
+  OVERCURRENT,
+  OVERVOLTAGE,
+  UNDERVOLTAGE,
+  ENCODER,
+  HARDWARE
+}
 
 /**
  * @brief AK60 Motor CAN Interface
@@ -50,12 +64,13 @@ protected:
   bool _shutdown;
   uint8_t _motor_id;
   MotorMode _motor_mode;
+  MotorFault _motor_fault;
 
   int64_t _command_period; //ms
   std::thread _can_writer;
-  std::thread _can_reader; //TO-DO
+  std::thread _can_reader;
 
-  void write_motor_position() {
+  void _setPosition() {
     struct can_frame wframe;
     wframe.can_id = CAN_EFF_FLAG | _motor_id | MotorMode::POSITION;
     int32_t pos_cmd_int = (int32_t) (cmd_angle * 10000.0f);
@@ -70,9 +85,38 @@ protected:
     }
   }
 
+  void read_motor_message() {
+    struct can_frame rframe;
+    int nbytes = read(_can_fd, &rframe, sizeof(struct can_frame));
+    if (nbytes < 0) {
+      perror("Error while reading from socket");
+    }
+    if (nbytes < sizeof(struct can_frame)) {
+      return;
+    }
+
+    position = ((int16_t) (rframe.data[0] << 8 | rframe.data[1])) / 10.0;
+    velocity = ((int16_t) (rframe.data[2] << 8 | rframe.data[3])) / 10.0;
+    current = ((int16_t) (rframe.data[4] << 8 | rframe.data[5])) / 100.0;
+    temperature = rframe.data[6];
+    _motor_fault = (MotorFault) rframe.data[7];
+  }
+
 public:
   float cmd_angle;
+  float current; //mA
+  float velocity; //rpm
+  float position; //deg
+  int8_t temperature; //C
 
+  /**
+   * @class AK60Manager
+   * @brief Class for managing AK60 motors over CAN interface.
+   *
+   * The AK60Manager class provides functionality to control AK60 motors
+   * using the CAN interface. It allows setting the motor mode, position,
+   * and reading motor messages.
+   */
   AK60Manager(const uint8_t motor_id, const char *can_interface) :
     cmd_angle(0.0),
     _motor_mode(MotorMode::POSITION),
@@ -111,7 +155,7 @@ public:
     rfilter.can_id = 0x00002900 | _motor_id;
     rfilter.can_mask = CAN_SFF_MASK;
     setsockopt(_can_fd, SOL_CAN_RAW, CAN_RAW_FILTER, &rfilter, sizeof(struct can_filter));
-  
+
     _can_writer = std::thread([this] {
       while (!_shutdown) {
         switch (_motor_mode)
@@ -125,7 +169,7 @@ public:
         case MotorMode::VELOCITY:
           continue;
         case MotorMode::POSITION:
-          write_motor_position();
+          _setPosition();
           continue;
         default:
           continue;
@@ -135,17 +179,32 @@ public:
     });
     _can_writer.detach();
 
-    /* TO-DO, add async _can_reader thread lambda*/
+    _can_reader = std::thread([this] {
+      while (!_shutdown) {
+        read_motor_message();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1.0f/400.0f));
+      }
+    });
+    _can_reader.detach();
   }
 
+  /**
+   * @brief Destructor for the AK60Manager class.
+   *
+   * This destructor sets the _shutdown flag to true and closes the _can_fd file descriptor.
+   */
   ~AK60Manager() {
     _shutdown = true;
     close(_can_fd);
   }
 
-
-  void stop() {
-    _shutdown = true;
+  /**
+   * @brief Get the current motor fault.
+   *
+   * @return MotorFault The current motor fault.
+   */
+  MotorFault getFault() {
+    return _motor_fault;
   }
 };
 
